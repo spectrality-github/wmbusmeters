28871d40aca8d1faaec4c7477297254e88c930e0
Spectrality <54081042+spectrality-github@users.noreply.github.com>
2023-09-28 20:33:01 +0300

    NZR driver

---
+++ src/driver_nzr.cc
@@ -0,0 +1,132 @@
+/*
+ Copyright (C) 2019-2022 Fredrik Öhrström (gpl-3.0-or-later)
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include"meters_common_implementation.h"
+
+namespace
+{
+    struct Driver : public virtual MeterCommonImplementation
+    {
+        Driver(MeterInfo &mi, DriverInfo &di);
+    };
+
+    static bool ok = registerDriver([](DriverInfo&di)
+    {
+        di.setName("nzr");
+        di.setDefaultFields("name,id,total_energy_consumption_kwh,current_power_consumption_kw,current_power_consumption_1_kw,current_power_consumption_2_kw,current_power_consumption_3_kw,voltage_at_phase_1_v,voltage_at_phase_2_v,voltage_at_phase_3_v");
+        di.setMeterType(MeterType::ElectricityMeter);
+        di.addLinkMode(LinkMode::T1);
+        di.addDetection(MANUFACTURER_NZR,  0x02,  0x00);
+        di.setConstructor([](MeterInfo& mi, DriverInfo& di){ return shared_ptr<Meter>(new Driver(mi, di)); });
+    });
+
+    Driver::Driver(MeterInfo &mi, DriverInfo &di) : MeterCommonImplementation(mi, di)
+    {
+        addNumericFieldWithExtractor(
+            "total_energy_consumption",
+            "The total energy consumption recorded by this meter.",
+            DEFAULT_PRINT_PROPERTIES,
+            Quantity::Energy,
+            VifScaling::Auto,
+            FieldMatcher::build()
+            .set(MeasurementType::Instantaneous)
+            .set(VIFRange::AnyEnergyVIF)
+            .set(TariffNr(1))
+            );
+
+        addNumericFieldWithExtractor(
+            "current_power_consumption",
+            "Current power consumption.",
+            DEFAULT_PRINT_PROPERTIES,
+            Quantity::Power,
+            VifScaling::Auto,
+            FieldMatcher::build()
+            .set(MeasurementType::Instantaneous)
+            .set(VIFRange::PowerW)
+            );
+        
+        addNumericFieldWithExtractor(
+            "current_power_consumption_1",
+            "Current power consumption L1.",
+            DEFAULT_PRINT_PROPERTIES,
+            Quantity::Power,
+            VifScaling::Auto,
+            FieldMatcher::build()
+            .set(DifVifKey("880128"))
+            );
+
+        addNumericFieldWithExtractor(
+            "current_power_consumption_2",
+            "Current power consumption L2.",
+            DEFAULT_PRINT_PROPERTIES,
+            Quantity::Power,
+            VifScaling::Auto,
+            FieldMatcher::build()
+            .set(DifVifKey("880228"))
+            );
+
+        addNumericFieldWithExtractor(
+            "current_power_consumption_3",
+            "Current power consumption L3.",
+            DEFAULT_PRINT_PROPERTIES,
+            Quantity::Power,
+            VifScaling::Auto,
+            FieldMatcher::build()
+            .set(DifVifKey("880328"))
+            );
+        
+        addNumericFieldWithExtractor(
+            "voltage_at_phase_1",
+            "Voltage at phase L1.",
+            DEFAULT_PRINT_PROPERTIES,
+            Quantity::Voltage,
+            VifScaling::None,
+            FieldMatcher::build()
+            .set(DifVifKey("8801FD40"))
+            );
+
+        addNumericFieldWithExtractor(
+            "voltage_at_phase_2",
+            "Voltage at phase L2.",
+            DEFAULT_PRINT_PROPERTIES,
+            Quantity::Voltage,
+            VifScaling::None,
+            FieldMatcher::build()
+            .set(DifVifKey("8802FD40"))
+            );
+
+        addNumericFieldWithExtractor(
+            "voltage_at_phase_3",
+            "Voltage at phase L3.",
+            DEFAULT_PRINT_PROPERTIES,
+            Quantity::Voltage,
+            VifScaling::None,
+            FieldMatcher::build()
+            .set(DifVifKey("8803FD40"))
+            );
+       
+
+    }
+}
+
+// Test: MyElectricity1 nzr 07911459 NOKEY
+// telegram=|4E4401061010101002027A00004005_2F2F0E035040691500000B2B300300066D00790C7423400C78371204860BABC8FC100000000E833C8074000000000BAB3C0000000AFDC9FC0136022F2F2F2F2F|
+// {"media":"electricity","meter":"nzr","name":"MyElectricity1","id":"10101010","total_energy_consumption_kwh":15694.05,"current_power_consumption_kw":0.33,"total_energy_production_kwh":7.48,"current_power_production_kw":0,"voltage_at_phase_1_v":236,"device_date_time":"2019-03-20 12:57:00","timestamp":"1111-11-11T11:11:11Z"}
+// |MyElectricity1;10101010;15694.05;0.33;7.48;0;236;null;null;null;null;null;null;null;null;1111-11-11 11:11.11
+
+
+
